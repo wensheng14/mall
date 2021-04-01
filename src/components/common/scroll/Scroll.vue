b8efbcb2b5868b6dd6e83f27bf507ec7f5cf48e8
wensheng <1733115718@qq.com>
2021-04-01 13:44:02 +0800

    'bug'

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="scroll">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Scroll"
+}
+</script>
+
+<style scoped>
+
+</style>
